228f9b403c2723a4e75144d2845a8b5c60e2704f
keanumatrix <keanumatrix1987@gmail.com>
2018-01-22 17:59:22 +0800

    1st time upload receive.py

---
+++ receive.py
@@ -0,0 +1,21 @@
+import serial,string,time
+output = ""
+ser = serial.Serial('/dev/serial0',9600,8,'N',1,timeout=1)
+
+while True:
+    
+    print 'waiting'
+    
+    #query = raw_input('Enter your input:')
+    
+    #ser.write(query + '\n')
+
+    time.sleep(1)
+    print "Buffer ="+str(ser.inWaiting())
+
+    if ser.inWaiting() >0:
+        output = ser.read(10)
+        print output
+        time.sleep(3)
+
+
